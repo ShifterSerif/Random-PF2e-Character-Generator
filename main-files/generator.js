ca77957f77c8a285dbcb61dfae9fd11306b4a302
Erick Sanchez <sanchez8erick@gmail.com>
2024-03-15 22:27:29 -0400

    Node Js
    Started work on Node.js version and now using the jsons for the character information

--- main-files/generator.js
+++ main-files/generator.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+
+
 let charLevel = 1;
 let spellcaster = false;
 let stats = new Array(6);
@@ -5,7 +8,6 @@ let profs = new Array(17);
 let skills = new Array(17);
 let saves = new Array(6);
 let spells = [];
-let feats = [];
 let genFeatAmount = 0;
 let skillFeatAmount = 0;
 let ancestryFeatAmount = 0;
@@ -13,27 +15,28 @@ let ancestry;
 let background;
 let charClass;
 
-function main(){
-    genFeatAmount = 0;
-    skillFeatAmount = 0;
-    charLevel = document.getElementById("userLevel").value;
-    ancestry = getAncestry();
-    background = getBackground();
-    charClass = getCharacterClass();
-    if (["Bard", "Cleric", "Druid", "Magus", "Oracle", "Psychic", "Ranger", "Sorcerer", "Summoner", "Witch", "Wizard"].includes(charClass)) {
-        spellcaster = true;
-    }
-    document.getElementById("Level").innerHTML = "Level: " + charLevel;
-    document.getElementById("Ancestry").innerHTML = "Ancestry: " + ancestry;
-    document.getElementById("Background").innerHTML = "Background: " + background;
-    document.getElementById("Class").innerHTML = "Class: " + charClass;
-    document.getElementById("Stats").innerHTML = "Stats: ";
-    document.getElementById("Feats").innerHTML = "Your feats are: ";
-    if(spellcaster){
-        document.getElementById("Spells").innerHTML = "Your spells are: ";
-    }
-    document.getElementById("Gender").innerHTML = "Your gender is: " + getGender();
+
+genFeatAmount = 0;
+skillFeatAmount = 0;
+ancestry = getAncestry();
+background = getBackground();
+charClass = getCharacterClass();
+if (["Bard", "Cleric", "Druid", "Magus", "Oracle", "Psychic", "Ranger", "Sorcerer", "Summoner", "Witch", "Wizard"].includes(charClass)) {
+    spellcaster = true;
 }
+console.log("Level: " + charLevel);
+console.log("Ancestry: " + ancestry);
+console.log("Background: " + background);
+console.log("Class: " + charClass);
+console.log("Stats: " + stats);
+console.log("Ancestry Feats: " + getAncestryFeats());
+console.log("Class Feats: " + getClassFeats());
+console.log("General Feats: " + getGeneralFeats());
+console.log("Skill Feats: " + getSkillFeats());
+if(spellcaster){
+    console.log("Spells: " + spells);
+}
+console.log("Your gender is: " + getGender());
 
 
 function getGender() {
@@ -42,18 +45,37 @@ function getGender() {
 }
 
 function getAncestry() {
-    let ancestries = ["Anadi", "Android", "Automaton", "Azarketi", "Catfolk", "Conrasu", "Dwarf", "Elf", "Fetchling", "Fleshwarp", "Ghoran", "Gnoll", "Gnome", "Goblin", "Goloma", "Grippli", "Halfling", "Hobgoblin", "Human", "Kashrishi", "Kitsune", "Kobold", "Leshy", "Lizardfolk", "Nagaji", "Orc", "Poppet", "Ratfolk", "Ratfolk", "Shisk", "Shoony", "Skeleton", "Sprite", "Strix", "Tengu", "Vanara", "Vishkanya"];
-    return ancestries[Math.floor(Math.random() * ancestries.length)];
+    const fileData = fs.readFileSync('aon-data/ancestries.json', 'utf8');
+    const ancestries = JSON.parse(fileData);
+    const randomIndex = Math.floor(Math.random() * ancestries.length);
+    const randomAncestry = ancestries[randomIndex].name;
+    return randomAncestry;
 }
+
 
 function getCharacterClass() {
-    let classes = ["Alchemist", "Barbarian", "Bard", "Champion", "Cleric", "Druid", "Fighter", "Investigator", "Kineticist", "Magus", "Monk", "Oracle", "Psychic", "Ranger", "Rogue", "Sorcerer", "Summoner", "Swashbuckler", "Thaumaturge", "Witch", "Wizard", "Gunslinger", "Inventor"];
-    return classes[Math.floor(Math.random() * classes.length)];
+    const fileData = fs.readFileSync('aon-data/classes.json', 'utf8');
+    const classes = JSON.parse(fileData);
+    const randomIndex = Math.floor(Math.random() * classes.length);
+    const randomClass = classes[randomIndex].name;
+    return randomClass;
 }
 
-function getBackground() { //not all backgrounds, just a proof of concept
-    let backgrounds = ["Acrobat", "Animal Whisperer", "Barkeep", "Barrister", "Blacksmith", "Bounty Hunter", "Criminal", "Entertainer", "Farmhand", "Field Medic", "Fortune Teller", "Gambler", "Gladiator", "Guard", "Herbalist", "Hermit", "Hunter", "Laborer", "Merchant", "Miner", "Noble", "Nomad", "Prisoner", "Sailor", "Scholar", "Scout", "Street Urchin", "Tinker", "Warrior", "Wasteland Scavenger"];
-    return backgrounds[Math.floor(Math.random() * backgrounds.length)];
+function getClassFeats() {
+    const fileData = fs.readFileSync('aon-data/classfeats/feats-' + charClass.toLowerCase() + '.json', 'utf8');
+    const classFeats = JSON.parse(fileData);
+    const randomIndex = Math.floor(Math.random() * classFeats.length);
+    const randomName = classFeats[randomIndex].name;
+    
+    return randomName + " " + classFeats[randomIndex].level;
+}
+
+function getBackground() {
+    const fileData = fs.readFileSync('aon-data/backgrounds.json', 'utf8');
+    const backgrounds = JSON.parse(fileData);
+    const randomIndex = Math.floor(Math.random() * backgrounds.length);
+    const randomBackground = backgrounds[randomIndex].name;
+    return randomBackground;
 }
 
 function getGeneralFeats() {
@@ -63,19 +85,30 @@ function getGeneralFeats() {
         tempCharLevel -= 4;
     }
     
+    const fileData = fs.readFileSync('aon-data/feats-general.json', 'utf8');
+    const generalFeats = JSON.parse(fileData);
+    const randomIndex = Math.floor(Math.random() * generalFeats.length);
+    const randomName = generalFeats[randomIndex].name;
+    
+    return randomName + " " + generalFeats[randomIndex].level;
 }
 
 function getSkillFeats() {
+    skillFeatAmount = charLevel;
     if (charClass != "Rogue") {
         let tempCharLevel = charLevel;
         while (tempCharLevel >= 2) {
             skillFeatAmount++;
             tempCharLevel -= 2;
         }
-    } else {
-        skillFeatAmount = charLevel;
-    }
-    return skillFeatAmount;
+    } 
+
+    const fileData = fs.readFileSync('aon-data/feats-skill.json', 'utf8');
+    const skillFeats = JSON.parse(fileData);
+    const randomIndex = Math.floor(Math.random() * skillFeats.length);
+    const randomName = skillFeats[randomIndex].name;
+    
+    return randomName + " " + skillFeats[randomIndex].level;
 }
 
 function getAncestryFeats() {
