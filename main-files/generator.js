45c8d46356d2b351a62c6ff7f39245db02bc6337
Erick Sanchez <sanchez8erick@gmail.com>
2024-03-17 21:47:24 -0400

    Improved Ability Boost Functionality

--- main-files/generator.js
+++ main-files/generator.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 let charLevel = 1;
 let spellcaster = false;
 let stats = [0, 0, 0, 0, 0, 0];
+let statNames = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"];
 let skills = ["Acrobatics", "Arcana", "Athletics", "Crafting", "Deception", "Diplomacy", "Intimidation", "Medicine", "Nature", "Occultism", "Performance", "Religion", "Society", "Stealth", "Survival", "Thievery", "Perception"];
 let profs = new Array(17);
 let saves = new Array(6);
@@ -14,23 +15,25 @@ let hp = 0;
 let ancestry;
 let background;
 let charClass;
+let keyAbility = "";
 
 genFeatAmount = 0;
 skillFeatAmount = 0;
-ancestry = getAncestry();
-background = getBackground();
 charClass = getCharacterClass();
+ancestry = getAncestry();
+// background = getBackground();
 ancestryFeats = getAncestryFeats();
 classFeats = getClassFeats();
 generalFeats = getGeneralFeats();
 skillFeats = getSkillFeats();
 gender = getGender();
-applyFreeBoosts();
+// useFreeBoosts();
 hp += stats[2] * charLevel;
 console.log("Level: " + charLevel);
 console.log("Ancestry: " + ancestry);
 console.log("Background: " + background);
 console.log("Class: " + charClass);
+console.log("Key Ability: " + keyAbility);
 console.log("Stats: STR " + stats[0] + " DEX " + stats[1] + " CON " + stats[2] + " INT " + stats[3] + " WIS " + stats[4] + " CHA " + stats[5]);
 console.log("HP: " + hp);
 console.log("Ancestry Feats: " + ancestryFeats);
@@ -40,9 +43,6 @@ console.log("Skill Feats: " + skillFeats);
 if(spellcaster){ console.log("Spells: " + spells); }
 console.log("Your gender is: " + gender);
 
-
-function applyFreeBoosts() {
-}
 
 function getGender() {
     let gender = ["Male", "Female", "Non-Binary"];
@@ -54,24 +54,66 @@ function getAncestry() {
     const ancestries = JSON.parse(fileData);
     const randomIndex = Math.floor(Math.random() * ancestries.length);
     const randomAncestry = ancestries[randomIndex].name;
+    
     hp += parseInt(ancestries[randomIndex].hp);
+    
+    let ancestryAbilityChoices = ancestries[randomIndex].ability_boost.split(", ");
+    let increasedKeyAbility = false;
+    statNames = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"];
+    if (ancestryAbilityChoices.includes(keyAbility)) {
+        increaseAbility(keyAbility);
+        statNames = statNames.filter(item => item !== keyAbility);
+        ancestryAbilityChoices = ancestryAbilityChoices.filter(item => item !== keyAbility);
+        increasedKeyAbility = true;
+    }
+    while(ancestryAbilityChoices.length > 0) {                          //NOT FULLY WORKING
+        let randomStat = Math.floor(Math.random() * statNames.length);
+        
+        if (ancestryAbilityChoices[0] != "Free") {
+            if (!increasedKeyAbility) {
+                increaseAbility(keyAbility);
+                increasedKeyAbility = false;
+                statNames = statNames.filter(item => item !== keyAbility);
+            }
+            else { increaseAbility(ancestryAbilityChoices[0]); }
+        } else {
+            increaseAbility(statNames[randomStat]);
+        }
+        statNames = statNames.filter(item => item !== randomStat);
+        ancestryAbilityChoices = ancestryAbilityChoices.slice(1);
+    }
+    
+    
+    let flaw = ancestries[randomIndex].ability_flaw;
+    if (flaw == "Strength") { stats[0] -= 1; }      
+    else if (flaw == "Dexterity") { stats[1] -= 1; }
+    else if (flaw == "Constitution") { stats[2] -= 1; }
+    else if (flaw == "Intelligence") { stats[3] -= 1; }
+    else if (flaw == "Wisdom") { stats[4] -= 1; }
+    else if (flaw == "Charisma") { stats[5] -= 1; }
+    
+    
     return randomAncestry;
 }
 
 function getCharacterClass() {
+    let keyAbilityChoices = [];
     const fileData = fs.readFileSync('aon-data/classes.json', 'utf8');
     const classes = JSON.parse(fileData);
     const randomIndex = Math.floor(Math.random() * classes.length);
     const randomClassName = classes[randomIndex].name;
-    console.log(classes[randomIndex].tradition)
+    
     if (classes[randomIndex].tradition != "") { spellcaster = true; }
+    
     hp += parseInt(classes[randomIndex].hp);
-    if (classes[randomIndex].ability == "Strength") { stats[0] += 1; }        // does not work for multi-ability choice classes
-    else if (classes[randomIndex].ability == "Dexterity") { stats[1] += 1; }
-    else if (classes[randomIndex].ability == "Constitution") { stats[2] += 1; }
-    else if (classes[randomIndex].ability == "Intelligence") { stats[3] += 1; }
-    else if (classes[randomIndex].ability == "Wisdom") { stats[4] += 1; }
-    else if (classes[randomIndex].ability == "Charisma") { stats[5] += 1; }
+    
+    if (classes[randomIndex].ability.includes(", ")) {
+        keyAbilityChoices = classes[randomIndex].ability.split(", ");
+        keyAbility = keyAbilityChoices[Math.floor(Math.random() * keyAbilityChoices.length)];
+    } else { keyAbility = classes[randomIndex].ability; }
+    increaseAbility(keyAbility); //increase from class
+    
+    
     return randomClassName;
 }
 
@@ -84,12 +126,50 @@ function getClassFeats() {
     return randomName + " " + classFeats[randomIndex].level;
 }
 
+function useFreeBoosts() {
+    increaseAbility(keyAbility); //increase from free boost
+    statNames = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"];
+    statNames = statNames.filter(item => item !== keyAbility);
+    let nextStat = statNames[Math.floor(Math.random() * statNames.length)];
+    increaseAbility(nextStat);
+    statNames = statNames.filter(item => item !== nextStat);
+    nextStat = statNames[Math.floor(Math.random() * statNames.length)];
+    increaseAbility(nextStat);
+    statNames = statNames.filter(item => item !== nextStat);
+    nextStat = statNames[Math.floor(Math.random() * statNames.length)];
+    increaseAbility(nextStat);
+}
+
+function increaseAbility(ability) {
+    if (ability == "Strength") { stats[0] += 1; }      
+    else if (ability == "Dexterity") { stats[1] += 1; }
+    else if (ability == "Constitution") { stats[2] += 1; }
+    else if (ability == "Intelligence") { stats[3] += 1; }
+    else if (ability == "Wisdom") { stats[4] += 1; }
+    else if (ability == "Charisma") { stats[5] += 1; }
+}
+
 function getBackground() {
     const fileData = fs.readFileSync('aon-data/backgrounds.json', 'utf8');
     const backgrounds = JSON.parse(fileData);
     const randomIndex = Math.floor(Math.random() * backgrounds.length);
-    const randomBackground = backgrounds[randomIndex].name;
-    return randomBackground;
+    const randomBackgroundName = backgrounds[randomIndex].name;
+    
+    let backgroundAbilityChoices = backgrounds[randomIndex].ability.split(", ");
+    
+    if (backgroundAbilityChoices.includes(keyAbility)) {
+        increaseAbility(keyAbility);
+        
+        statNames = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"];
+        statNames = statNames.filter(item => item !== keyAbility);
+        increaseAbility(statNames[Math.floor(Math.random() * statNames.length)]);
+    } else {
+        increaseAbility(backgroundAbilityChoices[Math.floor(Math.random() * backgroundAbilityChoices.length)]);
+        increaseAbility(keyAbility); //increase from background
+    }
+    
+    
+    return randomBackgroundName;
 }
 
 function getGeneralFeats() {
@@ -103,6 +183,11 @@ function getGeneralFeats() {
     const generalFeats = JSON.parse(fileData);
     const randomIndex = Math.floor(Math.random() * generalFeats.length);
     const randomName = generalFeats[randomIndex].name;
+    // for (let i = 0; i < generalFeats.length; i++) {
+    //     if (generalFeats[i].level == 7){
+    //         console.log(generalFeats[i].name)
+    //     }
+    // }
     
     return randomName + " " + generalFeats[randomIndex].level;
 }
